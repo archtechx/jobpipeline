6bc44803009a8f9ddc790477b4785d68b3e7d884
Zarunet <dev@zarunet.fr>
2025-07-19 22:41:07 +0200

    Add missing return type
    This adds the `self` return type to `JobPipeline::shouldBeQueued()` so that static analyzers won't get confused and treat its return value as `mixed`.

--- src/JobPipeline.php
+++ src/JobPipeline.php
@@ -51,7 +51,7 @@ class JobPipeline implements ShouldQueue
         return $this;
     }
 
-    public function shouldBeQueued(bool $shouldBeQueued = true, string $queue = null)
+    public function shouldBeQueued(bool $shouldBeQueued = true, string $queue = null): self
     {
         $this->shouldBeQueued = $shouldBeQueued;
 
